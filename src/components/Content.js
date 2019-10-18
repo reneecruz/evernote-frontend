9edaa139c3a62254d8f9e82fdfd8c35fe4cb1309
Renée Cruz <reneebasscruz@gmail.com>
2019-10-18 18:45:43 -0400

    conditional render of Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,11 +11,11 @@ import Instructions from './Instructions';
           refactor to get this Content component to work.
 */
 class Content extends Component {
-  renderContent = () => {
+  renderContent = (note) => {
     if (false) {
       return <NoteEditor />;
-    } else if (false) {
-      return <NoteViewer />;
+    } else if (true) {
+      return <NoteViewer note={this.props.note}/>;
     } else {
       return <Instructions />;
     }
@@ -24,8 +24,8 @@ class Content extends Component {
   render() {
     return (
       <div className='master-detail-element detail'>
-        { <NoteViewer note={this.props.note}/>}
-        {/*this.renderContent()*/}
+        { /*<NoteViewer note={this.props.note}/>*/}
+        {this.renderContent(this.note)}
       </div>
     );
   }
